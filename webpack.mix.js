e2a993397099a11091f7ba2c8f9e2d68b22747f6
mkdirmatias <mbaezar@ing.ucsc.cl>
2021-01-26 23:01:33 -0300

    include browsersync & alpinejs

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,7 +16,8 @@ mix.js('resources/js/app.js', 'public/js')
         require('postcss-import'),
         require('tailwindcss'),
         require('autoprefixer'),
-    ]);
+    ])
+    .browserSync('https://gameslist.test');
 
 if (mix.inProduction()) {
     mix.version();
